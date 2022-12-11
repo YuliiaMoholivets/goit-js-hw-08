fcdbf0afd1a30cbc391b685b69006f9ea922aa0a
YuliiaMoholivets <veronikaanddiana1@gmail.com>
2022-12-11 12:51:16 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createGalleryMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item"
+      target="_self"
+      rel="nofollow, noreferrer"
+      title="Click to enlarge" 
+      href="${original}">
+        <img class="gallery__image" 
+        src="${preview}" 
+        alt="${description}" />
+      </a>`;
+    })
+    .join('');
+}
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+}
+
+
+var lightbox = new SimpleLightbox('.gallery a', {
+
+  captionDelay: 250,
+  captionSelector: 'img',
+  captionsData: 'alt',
+});
