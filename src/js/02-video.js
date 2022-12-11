24b687685cc0492ec137c6c5cb0a7c044d686c97
YuliiaMoholivets <veronikaanddiana1@gmail.com>
2022-12-11 16:34:43 +0200

    parcel

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
 
+var throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
+
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+function currentTime({ seconds }) {
+  const timeToStart = Math.round(seconds);
+  localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, timeToStart);
+}
+
+let savedTime = localStorage.getItem(VIDEOPLAYER_CURRENT_TIME);
+savedTime = savedTime ? savedTime : 0;
+
+console.log(`Paused on: ${savedTime} second`);
+
+const throttleCurrentTime = throttle(currentTime, 1000);
+
+player.on('timeupdate', throttleCurrentTime);
+
+player.setCurrentTime(savedTime);
