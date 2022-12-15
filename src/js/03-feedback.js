3f94869303e5fe4fcd216036bb434cb02503c7eb
YuliiaMoholivets <veronikaanddiana1@gmail.com>
2022-12-15 15:46:56 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('feedback-form');
+const inputemail = document.querySelector('input[name=email]');
+const inputMessage = document.querySelector('textarea[name=message]');
+let formData = {};
+
+populateInput();
+
+form.addEventListener('input', throtle(setMessageToLocal, 500));
+form.addEventListener('submit', resetAndSubmitForm);
+
+function resetAndSubmitForm(e) {
+    e.preventDefault();
+    	const objSubmit = JSON.parse(localStorage.getItem(STORAGE_KEY))
+	console.log(objSubmit);
+	localStorage.removeItem(STORAGE_KEY);
+	formData = {};
+   form.reset();
+}
+function setMessageToLocal(e) {
+   const message = e.target.value;
+
+   formData[e.target.name] = message;
+   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateInput() {
+   const textForInput = localStorage.getItem(STORAGE_KEY);
+   const obj = JSON.parse(textForInput);
+   if (textForInput) {
+      if (obj.email) {
+         inputEmail.value = obj.email;
+      }
+      if (obj.message) {
+         inputMessage.value = obj.message;
+		}
+		formData = obj;
+   }
+}
