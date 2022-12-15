197b94dba722075c384e676ece0fb650ef13d76b
YuliiaMoholivets <veronikaanddiana1@gmail.com>
2022-12-15 13:57:48 +0200

    15-12

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-var throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
+const onPlay = function({seconds}) {
+    localStorage.setItem('videoplayer-current-time', seconds)
+};
 
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
-function currentTime({ seconds }) {
-  const timeToStart = Math.round(seconds);
-  localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, timeToStart);
-}
-
-let savedTime = localStorage.getItem(VIDEOPLAYER_CURRENT_TIME);
-savedTime = savedTime ? savedTime : 0;
-
-console.log(`Paused on: ${savedTime} second`);
-
-const throttleCurrentTime = throttle(currentTime, 1000);
-
-player.on('timeupdate', throttleCurrentTime);
-
-player.setCurrentTime(savedTime);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
